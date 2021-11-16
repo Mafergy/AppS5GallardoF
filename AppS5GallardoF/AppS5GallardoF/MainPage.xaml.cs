f54061991fc6e88b63fc281741eb3f064fb03f12
Fernanda Gallardo <Fernanda Gallardo@DESKTOP-6UVI768>
2021-11-16 15:35:10 -0500

    Agregar archivos de proyecto.

---
+++ AppS5GallardoF/AppS5GallardoF/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS5GallardoF
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.1.7/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<AppS5GallardoF.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            get();
+        }
+
+        public async void get()
+        {
+            try
+            {
+                var content = await client.GetStringAsync(Url);
+                List<AppS5GallardoF.Datos> posts = JsonConvert.DeserializeObject<List<AppS5GallardoF.Datos>>(content);
+                _post = new ObservableCollection<AppS5GallardoF.Datos>(posts);
+
+                MyListView.ItemsSource = _post;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", "ERROR" + ex.Message, "OK");
+            }
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new viewInsertar());
+
+        }
+
+        private void bntPost_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AppS5GallardoF/AppS5GallardoF/viewInsertar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS5GallardoF
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewInsertar : ContentPage
+    {
+        public viewInsertar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues("http://192.168.1.7/moviles/post.php","POST",parametros);
+                DisplayAlert("Mensaje", "Ingreso correcto", "ok");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Mensaje",ex.Message, "ok");
+
+            }
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());   
+        }
+    }
+}
